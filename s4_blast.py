a2ee53d3ac53d8fa5d37ded936838e5a00aa897d
B246942-2023 <B246942@github.com>
2023-11-13 19:20:21 +0000

    s4_blast.py updated : BAD VERSION made a menu , need another step to run blast

--- s4_blast.py
+++ s4_blast.py
@@ -115,7 +115,141 @@ if not os.path.exists(outfolder):
         os.makedirs(outfolder)
 
 # run blast menu
+#Menu
+while True:
+    #1 check the s4_out/seqBLAST_pre.txt status
+    file_path = 's4_out/seqBLAST_pre.txt'
+    #initialise the flags
+    flag_BLAST_LIST = False
+    flag_BLAST_runnings = False
+    flag_FINISH = False
+    if os.path.exists(file_path):
+        flag_BLAST_LIST = True
+        #check if there is a BLAST is running
+        with open(file_path,"r") as file:
+            content = file.read()
+            if "Running" in content:
+                flag_BLAST_runnings = True
+            if ("FINISH" in content) and (not flag_BLAST_runnings)  :
+                flag_FINISH = True
+    else:
+        flag_BLAST_LIST = False
 
+
+    #2 Menu it self
+    print("MENU_BLAST".center(100,"="))
+    print()
+    print(f"BLAST_LIST  Exist : {flag_BLAST_LIST}" .center(100))
+    print(f"BLAST Running State : {flag_BLAST_runnings}" .center(100))
+    print(f"BLAST FINISH State : {flag_FINISH}" .center(100))
+    print(f"    BLAST saved folder : s4_out" )
+    print()
+    print(f'''    1.Read the BLAST_LIST
+2.Make a BLAST_LIST
+3.Start BLAST   
+4.Check/Monitor BLAST State
+5.Reset and Delete
+6.Quit''')
+    print()
+    print("=".center(100,"="))
+
+    choices = input("Input your choices : ")
+    if choices == "1":
+        if flag_BLAST_LIST:
+            with open(file_path,"r") as f:
+                print(f.read())
+        else:
+            print()
+            print("No BLAST list , pls choose 2 to make one".center(100))
+            print() 
+        input("Press Enter to back the BLAST MENU".center(100,"-"))
+        continue
+    if choices == "2":
+        if flag_BLAST_runnings == False:
+            make_blast_list()
+            remove_duplicate_ids(file_path)
+        else:
+            print("There is a BLAST running now, can't make a new one".center(100))
+        continue
+    if choices == "3":
+        if flag_BLAST_runnings == True :
+            print()
+            print("BLAST is running , can't run new one")
+            print()
+        elif flag_BLAST_LIST == False:
+            print()
+            print("No BLAST list , pls choose 2 to make one".center(100))
+            print()
+        else:
+            #copy all fasta file to a new folder to blast
+            check_output_directory("s4_out/blast_copy")
+            for file in os.listdir("s3_out"):
+                if file.endswith(".fasta"):
+                    shutil.copy(f"s3_out/{file}",f"s4_out/blast_copy/{file}")
+            count = 0
+            while count <5: #run 5 blast at a same time
+                #need a function
+                time.sleep(0.3)
+                count+=1
+            print()
+            print("BLAST Start")
+            print()
+        input("Press Enter to continue".center(100,"-"))
+        continue
+    if choices == "4":
+        if flag_FINISH:
+            with open(file_path,"r") as f:
+                print(f.read())
+                input("Press Enter to back the BLAST MENU".center(100,"-"))
+        elif flag_BLAST_runnings:
+            continue_monitoring = True
+            while continue_monitoring:
+                print("Monitoring On".center(100,"="))
+                with open(file_path, "r") as file:
+                    content = file.read()
+                print(content)
+                print()
+                print("-".center(100,"-"))
+                out,error = linux_commands("ps -u $(whoami) -o pid,tty,time,cmd | grep [b]lastp | grep sh")
+                print(out)
+                print("-".center(100,"-"))
+                print()
+                print("Enter to Refresh".center(100))
+                stop = input("q to stop monitoring".center(100))
+                print()
+                print("-".center(100,"-"))
+                if stop == "q":
+                    continue_monitoring = False
+        else :
+            print()
+            print("No BLAST data or running process, pls start a BLAST".center(100)) 
+            print()
+            input("Press Enter to back the BLAST MENU".center(100,"-"))
+        continue
+
+    if choices == "5":
+        print()
+        if  flag_BLAST_runnings:
+            if input("IMPORTANT: BLAST is running , still reset?(y/n)").lower() == "y":
+                for i in range(5):
+                    linux_commands("pkill -f 'blastp' -u $(whoami)")
+                    print()
+                    print("RESETING".center(100))
+                    out,error = linux_commands("ps -u $(whoami) -o pid,tty,time,cmd | grep [b]lastp | grep sh")
+                    print(out)
+                    print()
+                    time.sleep(3)
+                check_output_directory( outfolder )
+                print("All BLAST data are delete".center(100,"-"))
+        else:
+            if input("Are you sure to delete ALL BLAST data?(y/n)").lower() == "y":
+                check_output_directory( outfolder )
+                print("All BLAST data are delete".center(100,"-"))
+        continue
+    if choices == "6":  
+        break
+    else:
+        print("Wrong input")
 
 
 
